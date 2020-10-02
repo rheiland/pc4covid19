21a48072b9a71211f7b468332a07d926ca1361da
Randy Heiland <heiland@indiana.edu>
2020-10-01 21:38:04 -0400

    update

--- data/flatten_covid19_cell_def_xml.py
+++ data/flatten_covid19_cell_def_xml.py
@@ -1,6 +1,10 @@
 # flatten_covid19_cell_def_xml.py - convert a PhysiCell_settings.xml with inheritance of <cell_definitions>
 #                                   into one without inheritance, i.e., verbose leaf <cell_definition>s.
 #
+# Note: leaf cell defs are hard-coded in this script for now.
+#
+# Author: Randy Heiland
+#
 
 import xml.etree.ElementTree as ET
 
@@ -11,30 +15,37 @@ xml_root = tree.getroot()
 #xml_root.text
 #xml_root.find('.//cell_definitions')
 
-leaf_cell_types = ["lung epithelium", "CD8 Tcell", "macrophage", "neutrophil", "DC", "CD4 Tcell"]
+leaf_cell_defs = ["lung epithelium", "CD8 Tcell", "macrophage", "neutrophil", "DC", "CD4 Tcell"]
 
-f = open('new_config.xml', 'w')
+# output file: new, flattened config file
+new_xml_file = 'new_config.xml'
+f = open(new_xml_file, 'w')
+
+# read the original config file into a string
 with open('PhysiCell_settings.xml', 'r') as file:
     data = file.read()
 
-default_start = data.index('<cell_definition ')
-print('default_start = ',default_start)
-default_end = data.index('</cell_definition>')
-print('default_end = ',default_end)
-default_str = data[default_start:default_end+19]
+default_start = data.index('<cell_definition ')  # find the start of the 'default' cell_def
+#print('default_start = ',default_start)
+default_end = data.index('</cell_definition>')   # find the end of the 'default' cell_def
+#print('default_end = ',default_end)
+default_str = data[default_start:default_end+19]   # put entire 'default' cell_def into a string
 
 user_params_start = data.index('<user_parameters>')
 
-f.write(data[:default_start])
-f.write(default_str)  # default
+f.write(data[:default_start])   # copy over everything up to the start of the 'default' cell_def
+f.write(default_str)  # copy the 'default'
 
 idx = 1
-for ctype in leaf_cell_types:
+# copy the 'default', but substitute the name and ID to be the leaf cell_defs
+for ctype in leaf_cell_defs:
     f.write('\t\t<cell_definition name="' + ctype + '"  ID="' + str(idx) + '">\n')
     f.write(default_str[40:])
     idx += 1
 f.write('\t</cell_definitions>\n\n')
 
+# append the <user_parameters> block, plus the terminating '</PhysiCell_settings>'
 f.write('\t' + data[user_params_start:])
 
 f.close()
+print("\nDone. Please check the output file: " + new_xml_file + "\n")
